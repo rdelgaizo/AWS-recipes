68d8b7bd9201999dcc3687ab76a5f300358b1f4c
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-06-03 13:31:34 -0400

    Update customize.rb

--- ruby/attributes/customize.rb
+++ ruby/attributes/customize.rb
@@ -1,2 +1,2 @@
-node.normal[:opsworks][:ruby_version] = '2.2.2'
-node.normal[:ruby][:patch_version] = '95'
+node.normal["opsworks"]["ruby_version"] = '2.2.2'
+node.normal["ruby"]["patch_version"] = '95'
