ce11e3a839058fdbe022dcdf1037c5497b5d1214
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-06-04 15:52:11 -0400

    Update extra.rb

--- ssh_users/libraries/extra.rb
+++ ssh_users/libraries/extra.rb
@@ -5,23 +5,22 @@ module OpsWorks
     def create_weddingwire_ng_group
       group 'weddingwire-ng' do
         action :create
+      end
     end
 
     def add_user_to_default_groups(params)
-        group "www-data" do
-          action :modify
-          members params[:name]
-          append true
-        end
+      group "www-data" do
+        action :modify
+        members params[:name]
+        append true
+      end
         
-        group 'weddingwire-ng' do
-          action :modify
-          members params[:name]
-          append true
-        end
-        
+      group 'weddingwire-ng' do
+        action :modify
+        members params[:name]
+        append true
+      end
     end
-
   end
 end
 
