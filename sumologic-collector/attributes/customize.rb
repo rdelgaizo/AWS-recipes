4a7887ed780356c61ed5bd038374076ac3c9944c
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-06-10 14:49:09 -0400

    Create customize.rb

---
+++ sumologic-collector/attributes/customize.rb
@@ -0,0 +1,3 @@
+# Data Bag for Collector Credentials
+normal['sumologic']['credentials']['bag_name'] = 'suxRgogIOtIWkA'
+normal['sumologic']['credentials']['item_name'] = 'XvnMPbWZeOgbvYDjT0mCJqwuYkQzyWlYLjrXUc901314KuU6IvuVZdPOLf9gNvMg'
