ae5e877b098b660be1b5399d447ed3be184bd842
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-13 13:48:28 -0400

    Delete customize.rb

--- sumologic-collector/attributes/customize.rb
+++
@@ -1,3 +0,0 @@
-# Data Bag for Collector Credentials
-normal['sumologic']['credentials']['bag_name'] = 'suxRgogIOtIWkA'
-normal['sumologic']['credentials']['item_name'] = 'XvnMPbWZeOgbvYDjT0mCJqwuYkQzyWlYLjrXUc901314KuU6IvuVZdPOLf9gNvMg'
