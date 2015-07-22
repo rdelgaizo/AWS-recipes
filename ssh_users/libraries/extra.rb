51e68ad3d719b4b511432b151f84caa409520290
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-22 14:02:37 -0400

    Update extra.rb

--- ssh_users/libraries/extra.rb
+++ ssh_users/libraries/extra.rb
@@ -2,6 +2,7 @@ module OpsWorks
   module User
     @@allocated_uids = []
     def create_weddingwire_ng_group
+      Chef::Log.info("Something something")
       #group 'weddingwire-ng' do
       #  action :create
       #  gid '3001'
@@ -9,12 +10,13 @@ module OpsWorks
       #end
     end
     def add_user_to_default_groups(params)
+      Chef::Log.info("Something something2")
       group "www-data" do
         action :modify
         members params[:name]
         append true
       end
-        
+      
       group 'weddingwire-ng' do
         action :modify
         members params[:name]
