087c2dc310441f2dc70098c84b2a7357d331ae15
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-23 12:00:27 -0400

    Update default.rb

--- ssh_users/recipes/default.rb
+++ ssh_users/recipes/default.rb
@@ -18,7 +18,7 @@ node[:ssh_users].each_key do |id|
       new_id = next_free_uid
       rename_user(existing_ssh_users[id], node[:ssh_users][new_id][:name])
       #added in to set the new users to the groups we want
-      Chef::Log.warn("Adding user for exisiting SSH user " + node[:ssh_users][new_id][:name])
+      Chef::Log.warn("Adding user for exisiting SSH user  #{node[:ssh_users][new_id][:name]}")
       add_user_to_default_groups(node[:ssh_users][id])
     end
   else
@@ -27,7 +27,7 @@ node[:ssh_users].each_key do |id|
     node.set[:ssh_users][id][:uid] = new_id
     setup_user(node[:ssh_users][id])
     #added in to set the new users to the groups we want
-    Chef::Log.warn("Adding user for new SSH user " + node[:ssh_users][new_id][:name])
+    Chef::Log.warn("Adding user for new SSH user #{node[:ssh_users][new_id][:name]}")
     add_user_to_default_groups(node[:ssh_users][id])
   end
   set_public_key(node[:ssh_users][id])
