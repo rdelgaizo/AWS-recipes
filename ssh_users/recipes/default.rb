163ea6120d34a148b6ce0e2d9b4a670d67cfe474
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-23 11:20:08 -0400

    Update default.rb

--- ssh_users/recipes/default.rb
+++ ssh_users/recipes/default.rb
@@ -1,14 +1,9 @@
-group 'extra'
 group 'opsworks'
 
-
 #added into create the new group we need
-Chef::Log.warn("Creating groups for instances")
-Chef::Log.error("Something something3")
-extra.create_weddingwire_ng_group
+create_weddingwire_ng_group
 
 existing_ssh_users = load_existing_ssh_users
-Chef::Log.info("Loaded existing users")
 existing_ssh_users.each do |id, name|
   unless node[:ssh_users][id]
     teardown_user(name)
@@ -21,7 +16,7 @@ node[:ssh_users].each_key do |id|
       new_id = next_free_uid
       rename_user(existing_ssh_users[id], node[:ssh_users][new_id][:name])
       #added in to set the new users to the groups we want
-      extra.add_user_to_default_groups(node[:ssh_users][id])
+      add_user_to_default_groups(node[:ssh_users][id])
     end
   else
     new_id = next_free_uid
@@ -29,11 +24,9 @@ node[:ssh_users].each_key do |id|
     node.set[:ssh_users][id][:uid] = new_id
     setup_user(node[:ssh_users][id])
     #added in to set the new users to the groups we want
-    extra.add_user_to_default_groups(node[:ssh_users][id])
+    add_user_to_default_groups(node[:ssh_users][id])
   end
   set_public_key(node[:ssh_users][id])
-  Chef::Log.info("Adding user to groups: #{node[:ssh_users][id][:name]}")
-  #add_user_to_default_groups(node[:ssh_users][id])
 end
 
 system_sudoer = case node[:platform]
