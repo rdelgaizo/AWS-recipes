2c9e66cc4bfe9a4ec15167e2c50e672f7b870dd9
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-23 15:31:05 -0400

    Update default.rb

--- ssh_users/recipes/default.rb
+++ ssh_users/recipes/default.rb
@@ -5,7 +5,7 @@ group 'extra'
 Chef::Log.warn("Creating groups")
 create_weddingwire_ng_group
 
-existing_ssh_users = load_existing_ssh_users
+existing_ssh_users = load_existing_ssh_users2
 existing_ssh_users.each do |id, name|
   Chef::Log.info("Checking #{id}  #{name}    #{node[:ssh_users][id]} ")
   unless node[:ssh_users][id]
