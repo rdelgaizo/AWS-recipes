00ec612e10d55b23640240e2733fe71afb69571f
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-23 15:51:11 -0400

    Update extra.rb

--- ssh_users/libraries/extra.rb
+++ ssh_users/libraries/extra.rb
@@ -6,10 +6,10 @@ module Extra
       return {} unless node[:opsworks_gid]
       existing_ssh_users = {}
       (node[:passwd] || node[:etc][:passwd]).each do |username, entry|
-        Chef::Log.warn("Checking #{username} with entry of #{entry}")
+        Chef::Log.error("Checking #{username} with entry of #{entry} and u_entry that gets logged as #{entry[:uid]}")
         if entry[:gid] == node[:opsworks_gid]
           Chef::Log.warn("Entry global id #{entry[:gid]} is equal to opsworks_gid #{node[:opsworks_gid]} with username #{username}")
-          existing_ssh_users[entry[:uid].to_s] = username
+          existing_ssh_users[entry[:uid].to_s] = username #stores as a hash with the user ID pointing to a username
         end
       end
       existing_ssh_users
