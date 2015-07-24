01f5877243c69e765203c9a41e88bd666b17ccb4
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-24 11:31:45 -0400

    Update extra.rb

--- ssh_users/libraries/extra.rb
+++ ssh_users/libraries/extra.rb
@@ -2,40 +2,6 @@ module Extra
   module User
     @@allocated_uids = []
     
-    def load_existing_ssh_users2
-      return {} unless node[:opsworks_gid]
-      existing_ssh_users = {}
-      
-      (node[:passwd] || node[:etc][:passwd]).each do |username, entry|
-        #Chef::Log.error("Checking #{username} with entry of #{entry} and u_entry that gets logged as #{entry[:uid]}")
-        if entry[:gid] == node[:opsworks_gid]
-          Chef::Log.warn("Entry global id #{entry[:gid]} is equal to opsworks_gid #{node[:opsworks_gid]} with username #{username}")
-          existing_ssh_users[entry[:uid].to_s] = username #stores as a hash with the user ID pointing to a username
-          Chef::Log.error("Node opsworks_gid is #{node[:opsworks_gid]}")
-        end
-      end
-      existing_ssh_users
-    end
-    
-    def kill_user_processes2(name)
-      Chef::Log.info("Killing all processes of user #{name}")
-      execute "kill all processes of user #{name}" do
-        command "pkill -u #{name}; true"
-      end
-    end
-
-    def teardown_user2(name)
-      Chef::Log.info("tearing down user #{name}")
-      kill_user_processes2(name)
-
-      user name do
-        action :remove
-        supports :manage_home => true
-      end
-    end
-    
-    
-    
     def create_weddingwire_ng_group
       group 'weddingwire-ng' do
         action :create
