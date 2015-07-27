f370f17d1e22a77bb27c3b79cc174764be17e8cb
rdelgaizo <rdelgaizo@users.noreply.github.com>
2015-07-27 10:01:04 -0400

    Update extra.rb

--- ssh_users/libraries/extra.rb
+++ ssh_users/libraries/extra.rb
@@ -7,8 +7,10 @@ module Extra
         action :create
         gid '3001'
       end
+      Chef::Log.warn("created weddingwire-ng group")
     end
     def add_user_to_default_groups(params)
+      Chef::Log.error("Add user #{params[:name]} to groups")
       group "www-data" do
         action :modify
         members params[:name]
